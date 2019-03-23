cb9e9608e7cb673b1cf69b1fd080e4c02cd8cfde
Mateusz Boczarski <matbocz@outlook.com>
2019-03-23 19:51:04 +0100

    Create emailEvent() function

--- PwJS_lab_4/js/forms.js
+++ PwJS_lab_4/js/forms.js
@@ -76,3 +76,24 @@ function surnameEvent() {
         document.getElementById("surname").className = "form-group has-success";
     }
 }
+
+function emailEvent() {
+    var contactEmail = document.getElementById("contactEmail");
+
+    if (contactEmail.value == "") {
+        document.getElementById("email").className = "form-group has-error";
+        document.getElementById("errorEmail").className = "alert alert-danger";
+        error = true;
+    } else {
+        var email = contactEmail.value;
+        var regex = /^[a-zA-Z0-9._-]+@([a-zA-Z0-9.-]+\.)+[a-zA-Z0-9.-]{2,4}$/;
+        if (regex.test(email) == false) {
+            document.getElementById("email").className = "form-group has-error";
+            document.getElementById("errorEmail").innerHTML = "Zły format adresu e-mail!";
+            document.getElementById("errorEmail").className = "alert alert-danger";
+            error = true;
+        } else {
+            document.getElementById("email").className = "form-group has-success";
+        }
+    }
+}
